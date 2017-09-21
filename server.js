d74f43725d5c620de612491534a3dd81be9c2858
Nidhi Shah <nidhi@Sohels-iMac.local>
2017-09-21 13:56:26 -0700

    prep for heroku

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require('express');
 
 // Create our app
-
 var app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next()
+  } else {
+    res.redirect('http://' + req.hostname + req.url)
+  }
+})
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
 
-  console.log('Server is up');
+  console.log('Server is up on port ' + PORT);
 });
